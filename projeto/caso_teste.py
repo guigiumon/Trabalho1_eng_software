9183d263e246f022968ccb833431ed37f0906f5f
Guilherme Giuliangeli Monteiro <guigiumon@gmail.com>
2026-05-08 16:42:48 -0300

    Variação dos valores de desconto

--- projeto/caso_teste.py
+++ projeto/caso_teste.py
@@ -3,7 +3,7 @@ from tipo_desconto import TipoDesconto
 
 maca = Produto.Produto(nome = "Maçã", preco =0.5, tipo_desconto = TipoDesconto.SEM_DESCONTO, valor_desconto=0)
 
-camisa = Produto.Produto(nome = "Maçã", preco =30.0, tipo_desconto = TipoDesconto.CUPOM, valor_desconto=0)
+camisa = Produto.Produto(nome = "Maçã", preco =30.0, tipo_desconto = TipoDesconto.CUPOM, valor_desconto=0.1)
 
-celular = Produto.Produto(nome = "Maçã", preco =1199.9, tipo_desconto = TipoDesconto.PERCENTUAL, valor_desconto=0)
+celular = Produto.Produto(nome = "Maçã", preco =1199.9, tipo_desconto = TipoDesconto.PERCENTUAL, valor_desconto=0.15)
 
