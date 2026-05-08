462d9551375577cb9db5e7f8ee97ca234e34d771
Guilherme Giuliangeli Monteiro <guigiumon@gmail.com>
2026-05-08 16:41:56 -0300

    Correção de erro na Criação de produtos

--- projeto/caso_teste.py
+++ projeto/caso_teste.py
@@ -1,9 +1,9 @@
 import Produto
 from tipo_desconto import TipoDesconto
 
-maca = Produto(nome = "Maçã", preco =0.5, tipo_desconto = TipoDesconto.SEM_DESCONTO, valor_desconto=0)
+maca = Produto.Produto(nome = "Maçã", preco =0.5, tipo_desconto = TipoDesconto.SEM_DESCONTO, valor_desconto=0)
 
-camisa = Produto(nome = "Maçã", preco =30.0, tipo_desconto = TipoDesconto.CUPOM, valor_desconto=0)
+camisa = Produto.Produto(nome = "Maçã", preco =30.0, tipo_desconto = TipoDesconto.CUPOM, valor_desconto=0)
 
-celular = Produto(nome = "Maçã", preco =1199.9, tipo_desconto = TipoDesconto.PERCENTUAL, valor_desconto=0)
+celular = Produto.Produto(nome = "Maçã", preco =1199.9, tipo_desconto = TipoDesconto.PERCENTUAL, valor_desconto=0)
 
